e56f642e32d951dd0bc38c405f5d69d4bcdac381
AnushreeDesai35 <anushreerdesai@gmail.com>
2017-02-11 08:55:12 -0800

    "module seperation"

--- oopjs/uiModule.js
+++ oopjs/uiModule.js
@@ -1,11 +1,20 @@
-// UI creator module
-
 var uiCreator = ( function() {
 	function createUI()
 	{
 		this.xmlui = function(xmlElements)
 		{
 			console.log(xmlElements);
+			for (i=0;i<xmlElements.length;i++){
+
+			if (xmlElements[i].tagName === 'panel')
+			{
+				drawDiv(xmlElements[i],"");
+			}
+			else
+			{
+				createElement(xmlElements[i],"");
+			}
+		}
 		};
 	}
   return createUI;
